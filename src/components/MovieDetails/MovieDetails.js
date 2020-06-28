16dff95b65d1cda375274f29c320d94584de128a
its-justus <ianjohnson9042@gmail.com>
2020-06-28 13:22:18 -0500

    added back and edit buttons to movie details

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {withRouter} from "react-router";
+import { withRouter } from "react-router";
 
 class MovieDetails extends Component {
+  back = () => {
+    this.props.history.goBack();
+  };
 
-	render() {
-		const {id, poster, title, description, genres} = this.props.details;
-		return (
-			<>
-				<h1>{title}</h1>
-				<img src={poster}/>
-				<p>{description}</p>
-				<ul>
-		{genres?.map((cur, i) => <li key={`genre-${i}`}>{cur}</li>)}
-				</ul>
-			</>
-		)
-	}
+  edit = () => {
+    this.props.history.push("/edit");
+  };
+
+  render() {
+    const { id, poster, title, description, genres } = this.props.details;
+    return (
+      <>
+        <button onClick={this.back}>Back to List</button>
+        <button onClick={this.edit}>Edit</button>
+        <h1>{title}</h1>
+        <img src={poster} />
+        <p>{description}</p>
+        <ul>
+          {genres?.map((cur, i) => (
+            <li key={`genre-${i}`}>{cur}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		details: state.details,
-	}
-}
+  return {
+    details: state.details,
+  };
+};
 
 export default withRouter(connect(mapStateToProps)(MovieDetails));
